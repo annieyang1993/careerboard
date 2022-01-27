bfe9cdfdc2f47ddfaa652e6f41b5138d02f046c5
Annie Yang <annieqiyang@gmail.com>
2022-01-27 18:29:56 -0500

    commit

--- firebase-hosting/frontend/components/app.jsx
+++ firebase-hosting/frontend/components/app.jsx
@@ -20,10 +20,10 @@ export default function App() {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route exact path="/">
+          <Route exact path="/jobs">
               <JobsContainer/>
             </Route>
-          <Route path="/newsletter">
+          <Route path="/">
               <NewsletterContainer/>
           </Route>
           <Route path="/postjob">
